bfaf12a70e81e7fabab125e96266fcc7313d32ba
Oskari Arvo <oskari.arvo@gmail.com>
2024-08-13 16:27:50 +0300

    Added some configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,39 @@
 import globals from "globals";
+import js from "@eslint/js";
+import stylisticJs from "@stylistic/eslint-plugin-js"
 
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  js.configs.recommended,
+  {
+    files: ["**/*.js"], 
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node, 
+      },
+      ecmaVersion: "latest",
+    },
+    plugins: {
+      "@stylistic/js": stylisticJs
+    },
+    rules: {
+      "@stylistic/js/indent": [
+        "error",
+        2
+      ],
+      "@stylistic/js/linebreak-style": [
+        "error",
+        "unix"
+      ],
+      "@stylistic/js/quotes": [
+        "error",
+        "single"
+      ],
+      "@stylistic/js/semi": [
+        "error",
+        "never"
+      ],
+    },
+  },
 ];
